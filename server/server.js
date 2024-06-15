4092d1b235c342c41fcfc45b6dcc28b63faa69ca
Shongkor <shongkortalukdar2017@gmail.com>
2024-06-15 16:09:32 +0600

    Port to 5050

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = require("./app");
 mongoose.connect(process.env.MONGO_URL).then(() => console.log("Database connection established"))
 .catch(() => console.log("Database connection failed"));
 
-const port = 5002;
+const port = 5050;
 
 app.listen(port, () => {
   console.log(`App is running at port 5002`);
