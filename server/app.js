be681a91c8e8b14afddc07058a175a761f8cf529
Shongkor <shongkortalukdar2017@gmail.com>
2024-06-21 14:50:23 +0600

    app.js

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.anpropertybd.com');
+  res.setHeader('Access-Control-Allow-Origin', 'http://www.antgec.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
