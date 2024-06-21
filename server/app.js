fda35765c373387361d2423a60c90e7d4a9f10a7
AnProperty <156059237+AnProperty@users.noreply.github.com>
2024-06-21 15:11:01 +0600

    Update app.js Access-Control-Allow-Origin

--- server/app.js
+++ server/app.js
@@ -20,13 +20,11 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://www.antgec.com');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+app.use(cors({
+  origin: ['http://antgec.com', 'http://www.antgec.com'],
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'], // Add PATCH and DELETE here
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use("/uploads", express.static("./uploads"));
 
