be01a719c88ff2d78a4562e2df39cdec89362312
Shongkor Talukdar <65601082+Shongkor@users.noreply.github.com>
2024-06-27 12:47:22 +0600

    Update TodaysStudents.js

--- client/src/views/Students/TodaysStudents/TodaysStudents.js
+++ client/src/views/Students/TodaysStudents/TodaysStudents.js
@@ -1,16 +1,6 @@
 import React from 'react';
 
 import {
-    CAvatar,
-    CButton,
-    CButtonGroup,
-    CCard,
-    CCardBody,
-    CCardFooter,
-    CCardHeader,
-    CCol,
-    CProgress,
-    CRow,
     CTable,
     CTableBody,
     CTableDataCell,
@@ -20,36 +10,13 @@ import {
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import {
-    cibCcAmex,
-    cibCcApplePay,
-    cibCcMastercard,
-    cibCcPaypal,
-    cibCcStripe,
-    cibCcVisa,
-    cibGoogle,
-    cibFacebook,
-    cibLinkedin,
-    cifBr,
-    cifEs,
-    cifFr,
-    cifIn,
-    cifPl,
-    cifUs,
-    cibTwitter,
-    cilCloudDownload,
     cilPeople,
-    cilUser,
-    cilUserFemale,
 } from '@coreui/icons'
-import avatar1 from 'src/assets/images/avatars/1.jpg'
-import avatar2 from 'src/assets/images/avatars/2.jpg'
-import avatar3 from 'src/assets/images/avatars/3.jpg'
-import avatar4 from 'src/assets/images/avatars/4.jpg'
-import avatar5 from 'src/assets/images/avatars/5.jpg'
-import avatar6 from 'src/assets/images/avatars/6.jpg'
+
 import { Link } from 'react-router-dom';
 
 const TodaysStudents = ({ students }) => {
+    const user = JSON.parse(localStorage.getItem('user'))
     return (
         <div>
             <CTable align="middle" className="mb-0 border" hover responsive>
@@ -74,15 +41,22 @@ const TodaysStudents = ({ students }) => {
                             <CTableDataCell className="text-center">{item.counselor.employee_name ? item.counselor.employee_name : "empty"}</CTableDataCell>
                             <CTableDataCell className="text-center">{item.status ? item.status : '0'}</CTableDataCell>
                             <CTableDataCell className="text-center">
-                                <Link to={`/super-admin/student-details/${item.studentId}/${item.counselor.employee_id
-                                    }`} state={{ item: item }}><button className="btn  btn-info bg-danger">More Info</button></Link>
+                                {
+                                    user.role === "receptionist" ? (
+                                        <Link to={`/receptionist/student-details/${item.studentId}/${item.counselor.employee_id
+                                            }`} state={{ item: item }}><button className="btn  btn-info bg-danger">More Info</button></Link>
+                                    ) : (
+                                        <Link to={`/super-admin/student-details/${item.studentId}/${item.counselor.employee_id
+                                            }`} state={{ item: item }}><button className="btn  btn-info bg-danger">More Info</button></Link>
+                                    )
+                                }
                             </CTableDataCell>
                         </CTableRow>
                     ))}
                 </CTableBody>
             </CTable>
 
-            
+
         </div>
     );
 };
